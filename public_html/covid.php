36aa5d3be4d0da32e45ea72f8e5f2a91a451ba00
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-05-23 20:35:28 -0700

    update covid policy

--- public_html/covid.php
+++ public_html/covid.php
@@ -15,9 +15,7 @@
     <h2>// Coronavirus Policy</h2>
     <p>
 Due to the Coronavirus, we are not certain whether or not our program will be in-person or online. We are holding off on
-collecting payment until we know more about Washington county’s plans to reopen, likely in June. If the program is
-online for health reasons, we will not be able to offer laptops, but it will be free and any payment accepted at that
-time will be fully refunded.
+collecting payment until we know more about Washington county’s plans to reopen, likely in June. If we have to move the program online, we will not be able to provide laptops, but we will provide around 2 hours of fun, interactive instruction on the same dates! Any payment accepted at that time will be fully refunded minus a credit card processing fee. We’ll keep you posted on developments.
     </p>
     <p>
 Register soon to reserve your spot and get a special early-bird price! Sign up <a href="https://bit.ly/bxbinterestform">here</a>
