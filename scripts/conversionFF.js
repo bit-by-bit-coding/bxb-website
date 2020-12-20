dd1cfa3288089d32cd7ac848bd7a67c8484223e8
EnneNerdyPerson <67206137+EnneNerdyPerson@users.noreply.github.com>
2020-12-19 18:15:08 -0800

    add google tracking tag to ff signup button

---
+++ scripts/conversionFF.js
@@ -0,0 +1,7 @@
+formAndFunctionElement = document.getElementById('ff-signup');
+
+formAndFunctionElement.onclick = conversionFF;
+
+function conversionFF(){
+    gtag('event', 'conversion', {'send_to': 'AW-458476450/wSX1CPqMm-4BEKKXz9oB'});
+}
