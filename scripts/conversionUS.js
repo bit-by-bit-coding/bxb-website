2417f95bddfd0d39ba3ffeb53ee122b240dc9757
EnneNerdyPerson <67206137+EnneNerdyPerson@users.noreply.github.com>
2020-12-19 18:15:21 -0800

    add google tracking tag to us signup button

---
+++ scripts/conversionUS.js
@@ -0,0 +1,7 @@
+uncommonSenseElement = document.getElementById('us-signup');
+
+uncommonSenseElement.onclick = conversionUS;
+
+function conversionUS(){
+    gtag('event', 'conversion', {'send_to': 'AW-458476450/wSX1CPqMm-4BEKKXz9oB'});
+}
