05bdbc63d79533b2b9d6a7b10832cc3d17aac117
Sydney Von Arx <error0x194@gmail.com>
2020-12-23 21:38:37 -0800

    remove console logs

--- scripts/global.js
+++ scripts/global.js
@@ -7,10 +7,8 @@ const menu = {
         menu.closeMenu();
         menu.open = e.currentTarget;
         $(e.currentTarget).addClass("open");
-        console.log(e.target);
     },
     closeMenu: () => {
-        console.log("closing time");
         menu.open = false;
         menu.$dropdowns.removeClass("open");
     },
@@ -29,7 +27,6 @@ $(window).resize(menu.setTriggers);
 $(document).click(menu.closeMenu);
 
 $(".dropdown-container").on('click mouseenter', (e) => {
-    console.log("stop!");
     e.stopPropagation();
 })
 
