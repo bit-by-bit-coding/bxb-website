74c7558796f9fc4db709d6fe9cdad37485f306e0
Sydney Von Arx <error0x194@gmail.com>
2020-12-29 00:25:29 -0800

    add global conversion tracking function

--- scripts/conversionFF.js
+++
@@ -1,7 +0,0 @@
-formAndFunctionElement = document.getElementById('ff-signup');
-
-formAndFunctionElement.onclick = conversionFF;
-
-function conversionFF(){
-    gtag('event', 'conversion', {'send_to': 'AW-458476450/wSX1CPqMm-4BEKKXz9oB'});
-}
--- scripts/conversionUS.js
+++
@@ -1,7 +0,0 @@
-uncommonSenseElement = document.getElementById('us-signup');
-
-uncommonSenseElement.onclick = conversionUS;
-
-function conversionUS(){
-    gtag('event', 'conversion', {'send_to': 'AW-458476450/wSX1CPqMm-4BEKKXz9oB'});
-}
