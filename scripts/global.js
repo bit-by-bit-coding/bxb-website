c1eed08f06167f999b0ed62875e6b51aaec5f99b
Sophia <sophia.wang2022@gmail.com>
2021-01-02 19:22:40 -0800

    Revert "JS w/ separate class for subdropdowns"
    This reverts commit ab7890a6f8155bc17825e0f9856f62968c123852.

--- scripts/global.js
+++ scripts/global.js
@@ -1,47 +1,32 @@
 const menu = {
     $dropdowns: $(".dropdown-container"),
-    $subdropdowns: $(".subdropdown-container"),
     open: false,
     openMenu: (e) => {
         //console.log($(e.target).hasClass("navbutton"));
         if (e.currentTarget === menu.open) return;
         e.stopPropagation()
         menu.closeMenu();
-        
         menu.open = e.currentTarget;
+
         $(e.currentTarget).addClass("open");
-    },
-    openSubmenu: (e) => {
-        if (e.currentTarget === menu.open) return;
-        menu.closeMenu();
-        menu.closeSubmenu();
-        menu.open = e.currentTarget;
-        $(e.currentTarget).addClass("open");
-        $(e.currentTarget).addClass("open2");
     },
     closeMenu: () => {
         menu.open = false;
         menu.$dropdowns.removeClass("open");
     },
-    closeSubmenu: () => {
-        menu.open = false;
-        menu.$subdropdowns.removeClass("open2");
-    },
     setTriggers: () => {
         menu.$dropdowns.off();
-        menu.$subdropdowns.off();
-        // if ($(window).width() <= 768) {
+        if ($(window).width() <= 768) {
             menu.$dropdowns.click(menu.openMenu);
-            menu.$subdropdowns.click(menu.openSubmenu);
-        // } else {
-        //     menu.$dropdowns.hover(menu.openMenu, menu.closeMenu);
-        // }
+        } else {
+            menu.$dropdowns.hover(menu.openMenu, menu.closeMenu);
+        }
     }
 }
+
 $(window).resize(menu.setTriggers);
 
 $(document).click(menu.closeMenu);
-$(document).click(menu.closeSubmenu);
 
 menu.setTriggers();
 
