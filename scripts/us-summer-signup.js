ced605f94e3f26b7e5e82ae8d7879d0488814d63
Sydney Von Arx <error0x194@gmail.com>
2021-01-23 23:02:19 -0800

    add encryption of US signup code

---
+++ scripts/us-summer-signup.js
@@ -0,0 +1,13 @@
+var AES = import("crypto-js/aes");
+
+const encryptedUrl = "U2FsdGVkX1/JVDLztyhFtQmHU5UDFJJgZQ5ih/AXfF1fKuLVVGx5avuHaI6IcpiWPKWu3BvlZXrAc6UgRBOG3an5MWCWPh9Ve1xXDFY9jB7a54CUGXur5wW+EpuViP4E5FdfS5Z8GNVmSghy1DzED1lXR1kBd+fvOGPIIdrHcNg=";
+
+$("#password-input").change(()=>{
+    const code = $("#password-input").val();
+    const url = AES.decrypt(encryptedUrl, code);
+    if(url.substring(0,8) === "https://"){
+        window.location.href = url;
+    } else {
+        alert("Sorry, that is not correct!");
+    }
+});
