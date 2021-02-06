3dd38cd11e755339afa3396c9b5afada787e776e
Sydney Von Arx <error0x194@gmail.com>
2021-02-05 20:47:45 -0800

    fix us signup encryption

--- scripts/us-summer-signup.js
+++ scripts/us-summer-signup.js
@@ -1,10 +1,11 @@
-var AES = import("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/aes.min.js");
+var AES = CryptoJS.AES;
 
 const encryptedUrl = "U2FsdGVkX1/JVDLztyhFtQmHU5UDFJJgZQ5ih/AXfF1fKuLVVGx5avuHaI6IcpiWPKWu3BvlZXrAc6UgRBOG3an5MWCWPh9Ve1xXDFY9jB7a54CUGXur5wW+EpuViP4E5FdfS5Z8GNVmSghy1DzED1lXR1kBd+fvOGPIIdrHcNg=";
 
 $("#password-input").change(()=>{
     const code = $("#password-input").val();
-    const url = AES.decrypt(encryptedUrl, code);
+    const url = AES.decrypt(encryptedUrl, code).toString(CryptoJS.enc.Utf8);
+
     if(url.substring(0,8) === "https://"){
         window.location.href = url;
     } else {
